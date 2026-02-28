c4d14e6093863c33926519d081e3ff5b2da76bd9
Rayane-Nait-Saidi <nr_nait_saidi@esi.dz>
2026-02-28 17:39:17 +0100

    Initialize the repo of our project

---
+++ frontend/myApp/app/(tabs)/index.tsx
@@ -0,0 +1,16 @@
+/*import WelcomePage from "@/components/WelcomePage";
+import { View , StyleSheet} from "react-native";
+
+export default function welcomepage(){
+    return(
+        <View style={styles.container}>
+          <WelcomePage/>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        backgroundColor:"white"
+    }
+})*/
---
+++ frontend/myApp/components/WelcomePage.tsx
@@ -0,0 +1,15 @@
+/*import {Text , View , StyleSheet} from "react-native"
+
+export default function WelcomePage(){
+    return(
+        <View style={styles.container}>
+          <Text>This is the welcome page!!</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        backgroundColor:"white",
+    }
+})*/
